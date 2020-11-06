ca14910bd6acc8a8ba711c30793770c1abcf725f
ejones12 <ejones12@student.strode-college.ac.uk>
2020-11-06 13:13:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def magnefication():
+  msize = int(input("type magnified size"))
+  osize = int(input("Type original size of atom")) 
+  mag = msize * 10000 / osize
+  print(mag,"%")
+
+
+magnefication()
